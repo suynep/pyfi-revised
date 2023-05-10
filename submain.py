d7fbd548e4628f4e974875e9f0e8550d39c43d49
suynep <suy.nepal@gmail.com>
2023-05-10 22:08:35 +0545

    added frame

--- submain.py
+++ submain.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+
 import urwid
 import os
+from datetime import datetime
 
 
 palette = [
@@ -15,15 +17,34 @@ palette = [
     ("text_highlight", "yellow,bold", ""),
 ]
 
+class TopFrame(urwid.Columns):
+    def __init__(self, browser_panel_list):
+        self.panels = browser_panel_list
+        self.focus_arr = []
+        super().__init__(self.panels, dividechars=1)
+
+    def keypress(self, size, key):
+        if key in ['h']:
+            self.focus_position = (self.focus_position - 1) % (len(self.panels))
+            self.focus_arr.append(self.focus)
+            return key
+            
+        elif key in ['l']:
+            self.focus_position = (self.focus_position + 1) % (len(self.panels))
+            return key
+        else:
+            return key
+
 
 class BrowserPanel(urwid.ListBox):
+
     def __init__(self, path):
         self.current_path = path
         self.root_path = '/'
         self.previous_path = self.root_path
         self.hidden = False
-        walker = self.show_buttons()
-        super().__init__(walker)
+        list_box = self.show_buttons()
+        super().__init__(list_box)
 
     def gen_file_list(self):
         all_files = os.listdir(self.current_path)
@@ -56,7 +77,6 @@ class BrowserPanel(urwid.ListBox):
         for f in self.gen_file_list():
             button = urwid.Button(f)
             pathvar = os.path.join(self.current_path, f)
-            print(os.path.split(pathvar))
             if (os.path.isdir(pathvar)):
                 urwid.connect_signal(button, 'click', self.update_list, pathvar)
                 body.append(urwid.AttrMap(button, 'dirs', focus_map='dirsrev'))
@@ -72,18 +92,33 @@ class BrowserPanel(urwid.ListBox):
         self.current_path = dirpath
 
 
-
-
 def exit_on_q(key):
     if key in ['q']:
         raise urwid.ExitMainLoop()
 
+
+def create_frame(panel):
+    title = "PyFi: A Terminal File Manager"
+    today = datetime.now()
+    today_f  = today.strftime("%d/%m/%Y %H:%M:%S")
+    header_t = urwid.AttrMap(urwid.Text(('text_highlight', f"{title}")), 'reveal_focus')
+    footer_t = urwid.AttrMap(urwid.Text(('text_highlight', f"{today_f}")), 'reveal_focus')
+    main_frame = urwid.Frame(panel, header=header_t, footer=footer_t)
+    return main_frame
+
+
 def main():
+    browser_panels = []
+    for i in range(2):
+        browser_panels.append(BrowserPanel(os.path.expanduser('~')))
 
-    p = BrowserPanel(os.path.expanduser('~'))
+    top_frame = TopFrame(browser_panels)
 
-    loop = urwid.MainLoop(p, palette, unhandled_input=exit_on_q)
+    main_frame = create_frame(top_frame)
+    loop = urwid.MainLoop(main_frame, palette, unhandled_input=exit_on_q)
     loop.run()
+    print(main_frame._selectable)
 
+    
 if __name__ == '__main__':
     main()
